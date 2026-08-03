b8671f0d6bf1688e3c9814ce0b5dadef466ce2f6
James Litchfield <james.litchfield99@gmail.com>
2026-08-03 12:47:08 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import '../styles/app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+
+<header>
+
+</header>
+
+<div class="rounded-xl bg-black">
+	<main>
+	
+	</main>
+</div>
+
+{@render children()}
