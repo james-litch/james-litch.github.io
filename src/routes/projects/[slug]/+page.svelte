bcedcbb641c610b6a2b8f8dd7ba3009bafb65492
James Litchfield <james.litchfield99@gmail.com>
2026-08-11 00:03:34 +0100

    Add in skills section to projects page

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -17,6 +17,17 @@
 
 <p class='mb-4'>{data.metadata.description}</p>
 
+{#if data.metadata.tech?.length}
+  <section class='space-y-2 mb-4'>
+    <h2 class='text-fluid-lg font-bold mb-'>Tech Stack</h2>
+    <ul class='flex flex-wrap gap-2 text-fluid-sm font-medium'>
+      {#each data.metadata.tech as tool (tool)} 
+        <li class='px-2 py-1 bg-surface-2 rounded-md'>{tool}</li>
+      {/each}
+    </ul>
+  </section>
+{/if}
+
 {#if url || github}
   <div class="mb-6">
     <h2 class='text-fluid-xl font-bold mb-4'>Links</h2>
