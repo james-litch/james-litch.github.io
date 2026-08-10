ba539b0bb76f3a47a1919f8570e5ab0aafdefa31
James Litchfield <james.litchfield99@gmail.com>
2026-08-10 19:46:07 +0100

    Fix projects layout on mobile
    - move links to subheading to avoid the ui feeling cluttered

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -9,31 +9,35 @@
   let url = $derived(data.metadata.url);
 </script>
 
-<div class='flex justify-between gap-x-10 gap-y-4 items-center'>
-  <a href={resolve('/projects')} class='flex gap-1 hover:underline items-center text-sm text-text-secondary font-medium'>
-    <IconArrowLeft width="1.5em" height="1.5em"/> Back to Projects
-  </a>
-  <div class="flex gap-2 flex-wrap">
-    {#if url}
-      <a href={url} rel='external' class='button-primary text-fluid-xs' target='_blank'>
-        <IconExternal width="1em" height="1em"/>
-        Live
-      </a>
-    {/if}
-
-    {#if github}
-      <a href={github} rel='external' class='button-secondary text-fluid-xs items-center' target='_blank'>
-        <IconGitHub width="1em" height="1em"/>
-        GitHub
-      </a>
-    {/if}
-  </div>
-</div>
-
+<a href={resolve('/projects')} class='flex gap-1 hover:underline items-center text-sm text-text-secondary font-medium mb-3'>
+  <IconArrowLeft width="1.5em" height="1.5em"/> Back to projects
+</a>
 
 <h1 class='accented text-fluid-2xl font-bold mb-6'>{data.metadata.title}</h1>
 
 <p class='mb-4'>{data.metadata.description}</p>
+
+{#if url || github}
+  <div class="mb-6">
+    <h2 class='text-fluid-xl font-bold mb-4'>Links</h2>
+    <div class="flex gap-2 flex-wrap">
+      {#if url}
+        <a href={url} rel='external' class='button-primary text-fluid-xs' target='_blank'>
+          <IconExternal width="1em" height="1em"/>
+          Live
+        </a>
+      {/if}
+
+      {#if github}
+        <a href={github} rel='external' class='button-secondary text-fluid-xs items-center' target='_blank'>
+          <IconGitHub width="1em" height="1em"/>
+          GitHub
+        </a>
+      {/if}
+    </div>
+  </div>
+{/if}
+
 
 <div class='prose'>
   <data.content />
